11c49363f27f0a75109fe66b0492f7324f367479
Steffen Svenningsen <stef7625@gmail.com>
2024-04-02 17:18:36 +0200

    Project 1

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <router-link to="/todo-app">Todo-app</router-link>
+  </nav>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
-<script setup></script>
+<script setup>
+import NavigationMenu from '@/components/NavigationMenu.vue'
+</script>
 
 <template>
   <h1>Home</h1>
+  <NavigationMenu />
 </template>
